91be02e01f6d0f9cfd3ed90208400c75baeb53cf
mcfuzzy22 <mcfuzzy22@gmail.com>
2024-10-28 19:00:47 -0400

    First commit

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,19 @@
+
+/**
+ * Created by PhpStorm.
+ * User: mcfuzzy22
+ * Date: 10/27/2024
+ * Time: 11:32 PM
+ */
+!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+
+    <title>Document</title>
+</head>
+<body>
+    <h1>All Users</h1>
+</body>
+</html>
